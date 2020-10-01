9f4b0fec8fb29a4f33bdfd52a9bfbe5106849a6e
Vignesh Hariharan Iyer <vigneshiyer666666@gmail.com>
2020-10-01 13:39:03 +0530

    Give appropriate spacing in loops.
    It is always better to keep some spacing. The for loop within the for loop can be seen more clearly because of this spacing.

--- 19_transpose_of_matrix.c
+++ 19_transpose_of_matrix.c
@@ -5,30 +5,33 @@ int main()
 int m, n, a, d, matrix[10][10], transpose[10][10];
 printf("Enter the number of rows and columns of matrix \n ");
 scanf("%d%d",&m,&n); printf("Enter the elements of matrix \n");
+/*Input Matrix*/  
 for( a = 0 ; a < m ; a++ )
-{
-for( d = 0 ; d < n ; d++ )
-{
-scanf("%d",&matrix[a][d]);
-}
+  {
+    for( d = 0 ; d < n ; d++ )
+     {
+      scanf("%d",&matrix[a][d]);
+     }
 }
 
+
+/*Perform Transposing*/
 for( a = 0 ; a < m ; a++ )
-{
-for( d = 0 ; d < n ; d++ )
-{
-transpose[d][a] = matrix[a][d];
-/* transpose by interchanging rows and columns */
-}
+  {
+    for( d = 0 ; d < n ; d++ )
+    {
+      transpose[d][a] = matrix[a][d]; //Assigning Value
+    }
 }
 
+/*Printing Transpose of input matrix*/
 printf("Transpose of entered matrix :-\n");
 for( a = 0 ; a < n ; a++ )
-{
-for( d = 0 ; d < m ; d++ )
-{
-printf("%d\t",transpose[a][d]);
-}
+    {
+      for( d = 0 ; d < m ; d++ )
+      {
+       printf("%d\t",transpose[a][d]); 
+      }
 printf("\n");
 }
 return 0;
